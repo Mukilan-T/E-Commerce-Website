ef97391409f7075d0c9d372a8f81679d2592f59c
Mukilan-T <mukilanthangadurai@gmail.com>
2023-04-29 07:17:22 +0530

    align_done

--- client/src/components/CategoryItem.jsx
+++ client/src/components/CategoryItem.jsx
@@ -1,7 +1,7 @@
 
 import styled from "styled-components";
 import { mobile } from "../Responsive";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 const Container = styled.div`
@@ -15,7 +15,7 @@ const Image = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
-  ${mobile({height: "30vh"})}
+  ${mobile({ height: "30vh" })}
 
 `;
 
@@ -48,12 +48,12 @@ const Button = styled.button`
 const CategoryItem = ({ item }) => {
   return (
     <Container>
-     <Link to={`/products/${item.cat}`}>
-      <Image src={item.img} />
-      <Info>
-        <Title>{item.title}</Title>
-        <Button>SHOP NOW</Button>
-      </Info>
+      <Link to={`/products/${item.cat}`}>
+        <Image src={item.img} />
+        <Info>
+          <Title>{item.title}</Title>
+          <Button>SHOP NOW</Button>
+        </Info>
       </Link>
     </Container>
   );
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,38 +3,38 @@ import { Snackbar } from "@mui/material";
 import styled from "styled-components"
 import { mobile } from "../Responsive";
 
-const Container=styled.div`
+const Container = styled.div`
     display:flex;
-    ${mobile({flexDirection: "column"})}
+    ${mobile({ flexDirection: "column" })}
 `;
 
-const Left=styled.div`
+const Left = styled.div`
     flex:1;
     display:flex;
     flex-direction: column;
     padding:20px;
 `;
 
-const Logo=styled.h1`
+const Logo = styled.h1`
 
 `;
 
-const Desc=styled.p`
+const Desc = styled.p`
     margin:20px 0px;
 `;
 
-const SocialContainer=styled.div`
+const SocialContainer = styled.div`
     display:flex;
     padding:10px;
     
 `;
 
-const SocialIcon=styled.div`
+const SocialIcon = styled.div`
     width:40px;
     height: 40px;
     border-radius: 50%;
     color:white;
-    background-color:#${props=>props.color};
+    background-color:#${props => props.color};
     display:flex;
     align-items:center;
     justify-content: center;
@@ -42,18 +42,18 @@ const SocialIcon=styled.div`
 
 `;
 
-const Center=styled.div`
+const Center = styled.div`
     flex:1;
     padding:20px;
-    ${mobile({display: "none"})}
+    ${mobile({ display: "none" })}
 `;
 
-const Title=styled.h3`
+const Title = styled.h3`
     margin-bottom: 30px;
 
 `;
 
-const List=styled.ol`
+const List = styled.ol`
     margin:0;
     padding: 0;
     list-style:none;
@@ -66,87 +66,87 @@ const ListItem = styled.div`
     margin-bottom:10px;
 `;
 
-const Right=styled.div`
+const Right = styled.div`
     flex:1;
     padding:20px;
-    ${mobile({backgroundColor: "#fff8f8"})}
+    ${mobile({ backgroundColor: "#fff8f8" })}
 `;
 
-const ContactItem=styled.div`
+const ContactItem = styled.div`
     margin-bottom: 20px;
     display: flex;
     align-items: center;
 `;
 
-const Payment=styled.img`
+const Payment = styled.img`
     width: 50%;
 `;
 
 const Footer = () => {
-  return (
-    <Container>
-        <Left>
-            <Logo>TRendZ... </Logo>
-            <Desc>There are many variations of passages of Lorem Ipsum available, but
-            the majority have suffered alteration in some form, by injected
-            humour, or randomised words which don’t look even slightly believable. </Desc>
-        <SocialContainer>
-            <SocialIcon color="3B5999">
-                <Facebook/>
-            </SocialIcon>
+    return (
+        <Container>
+            <Left>
+                <Logo>TRendZ... </Logo>
+                <Desc>There are many variations of passages of Lorem Ipsum available, but
+                    the majority have suffered alteration in some form, by injected
+                    humour, or randomised words which don’t look even slightly believable. </Desc>
+                <SocialContainer>
+                    <SocialIcon color="3B5999">
+                        <Facebook />
+                    </SocialIcon>
 
-            
-            <SocialIcon color="E4405F">
-                <Instagram/>
-            </SocialIcon>
 
-            
-            <SocialIcon color="55ACEE">
-                <Twitter/>
-            </SocialIcon>
+                    <SocialIcon color="E4405F">
+                        <Instagram />
+                    </SocialIcon>
 
-            
-            <SocialIcon color="E60023">
-                <YouTube/>
-            </SocialIcon>
 
-            
-            <SocialIcon color="3F5829">
-                <Pinterest/>
-            </SocialIcon>
+                    <SocialIcon color="55ACEE">
+                        <Twitter />
+                    </SocialIcon>
 
-        </SocialContainer>
-        </Left>
-        <Center>
-            <Title>Useful Links</Title>
-            <List>  
-            <ListItem>Home</ListItem>
-            <ListItem>Cart</ListItem>
-            <ListItem>Man Fashion</ListItem>
-            <ListItem>Woman Fashion</ListItem>
-            <ListItem>Accessories</ListItem>
-            <ListItem>My Account</ListItem>
-            <ListItem>Order Tracking</ListItem>
-            <ListItem>Wishlist</ListItem>
-            <ListItem>Wishlist</ListItem>
-            <ListItem>Terms</ListItem>
-            </List>
-        </Center>
-        <Right>
-            <Title>Contact</Title>
-        <ContactItem>
-            <Room style={{marginRight:"10px"}}/> 622 Dixie Path , South Tobinchester 98336
-        </ContactItem>
-        <ContactItem>
-            <Phone style={{marginRight:"10px"}}/> +1 234 56 78
-        </ContactItem>
-        <ContactItem>
-            <MailOutline style={{marginRight:"10px"}} /> contact@lama.dev
-        </ContactItem>
-            <Payment src="https://iili.io/H786jig.jpg" />
-        </Right>
-        
-    </Container>
+
+                    <SocialIcon color="E60023">
+                        <YouTube />
+                    </SocialIcon>
+
+
+                    <SocialIcon color="3F5829">
+                        <Pinterest />
+                    </SocialIcon>
+
+                </SocialContainer>
+            </Left>
+            <Center>
+                <Title>Useful Links</Title>
+                <List>
+                    <ListItem>Home</ListItem>
+                    <ListItem>Cart</ListItem>
+                    <ListItem>Man Fashion</ListItem>
+                    <ListItem>Woman Fashion</ListItem>
+                    <ListItem>Accessories</ListItem>
+                    <ListItem>My Account</ListItem>
+                    <ListItem>Order Tracking</ListItem>
+                    <ListItem>Wishlist</ListItem>
+                    <ListItem>Wishlist</ListItem>
+                    <ListItem>Terms</ListItem>
+                </List>
+            </Center>
+            <Right>
+                <Title>Contact</Title>
+                <ContactItem>
+                    <Room style={{ marginRight: "10px" }} /> 622 Dixie Path , South Tobinchester 98336
+                </ContactItem>
+                <ContactItem>
+                    <Phone style={{ marginRight: "10px" }} /> +1 234 56 78
+                </ContactItem>
+                <ContactItem>
+                    <MailOutline style={{ marginRight: "10px" }} /> contact@lama.dev
+                </ContactItem>
+                <Payment src="https://iili.io/H786jig.jpg" />
+            </Right>
+
+        </Container>
     )
 }
 
