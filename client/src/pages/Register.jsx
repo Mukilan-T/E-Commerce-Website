9db67aec324daf668f389bcfac0ed25b1083d114
Mukilan-T <mukilanthangadurai@gmail.com>
2023-04-29 07:13:20 +0530

    register_up

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 import { mobile } from "../Responsive";
 
-const Container=styled.div`
+const Container = styled.div`
         width : 100vw;
         height : 100vh;
         background: linear-gradient(
@@ -16,24 +16,24 @@ const Container=styled.div`
 
 `;
 
-const Wrapper=styled.div`
+const Wrapper = styled.div`
         width: 40%;
         padding: 20px;
         background-color: white;
-        ${mobile({width: "75%"})}
+        ${mobile({ width: "75%" })}
 `;
 
-const Title=styled.h1`
+const Title = styled.h1`
         font-size: 24px;
         font-weight: 300;
 `;
 
-const Form=styled.form`
+const Form = styled.form`
         display: flex;
         flex-wrap: wrap;
 `;
 
-const Input=styled.input`
+const Input = styled.input`
         flex: 1;
         min-width: 40%;
         margin: 20px 10px 0px 0px; 
@@ -41,12 +41,12 @@ const Input=styled.input`
 
 `;
 
-const Agreement=styled.span`
+const Agreement = styled.span`
         font-size: 12px;
         margin: 20px 0px;
 `;
 
-const Button=styled.button`
+const Button = styled.button`
         width: 40%;
         border: none;
         padding: 15px 20px;
@@ -56,28 +56,28 @@ const Button=styled.button`
 `;
 
 const Register = () => {
-  return (
-    <Container>
-        <Wrapper>
-            <Title>
-                Create an Account
-            </Title>
-            <Form>
-                <Input placeholder="Name"/>
-                <Input placeholder="Last Name"/>
-                <Input placeholder="User Name"/>
-                <Input placeholder="Email"/>
-                <Input placeholder="Password"/>
-                <Input placeholder="Confirm Password"/>
-                <Agreement> 
-                By creating an account, I consent to the processing of my personal
-            data in accordance with the <b>PRIVACY POLICY</b>
-                </Agreement>
-                <Button>CREATE</Button>
-            </Form>
-        </Wrapper>
-        </Container>
-  )
+        return (
+                <Container>
+                        <Wrapper>
+                                <Title>
+                                        Create an Account
+                                </Title>
+                                <Form>
+                                        <Input placeholder="Name" />
+                                        <Input placeholder="Last Name" />
+                                        <Input placeholder="User Name" />
+                                        <Input placeholder="Email" />
+                                        <Input placeholder="Password" />
+                                        <Input placeholder="Confirm Password" />
+                                        <Agreement>
+                                                By creating an account, I consent to the processing of my personal
+                                                data in accordance with the <b>PRIVACY POLICY</b>
+                                        </Agreement>
+                                        <Button>CREATE</Button>
+                                </Form>
+                        </Wrapper>
+                </Container>
+        )
 }
 
 export default Register
